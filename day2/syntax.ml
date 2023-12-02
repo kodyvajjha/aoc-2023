0abd50956022950c27bbf8707a6bab7d9f332d57
Koundinya Vajjha <koundinya.vajjha@gmail.com>
2023-12-02 14:29:30 -0800

    cleanup

--- day2/syntax.ml
+++ day2/syntax.ml
@@ -1,17 +1,9 @@
-(* type cubes =
-     | Red of int
-     | Green of int
-     | Blue of int
-   [@@deriving show { with_path = false }] *)
-
 type cubes = {
   red: int option;
   green: int option;
   blue: int option;
 }
 [@@deriving show { with_path = false }]
-
-(* type grab = cubes list [@@deriving show { with_path = false }] *)
 
 type game = {
   id: int;
