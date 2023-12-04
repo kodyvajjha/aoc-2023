7f1cf745eeb8bfd8c004b4e8af4e48f7ca7c5781
Koundinya Vajjha <koundinya.vajjha@gmail.com>
2023-12-04 12:14:29 -0800

    part1 day 4

--- day4/day4.ml
+++ day4/day4.ml
@@ -28,21 +28,45 @@ module Syntax = struct
 
   let _parse_string ~input =
     let open CCParse in
-    let string_parser = each_line parser >|= CCList.keep_some in
+    let string_parser =
+      each_line (parsing "string" parser) >|= CCList.keep_some
+    in
     try parse_string_exn string_parser input
     with ParseError err ->
       failwith @@ CCFormat.sprintf "failed: %a" CCParse.Error.pp err
 
   let parse_file ~input =
     let open CCParse in
-    let file_parser = each_line (parsing "line" parser) >|= CCList.keep_some in
+    let file_parser = each_line (parsing "file" parser) >|= CCList.keep_some in
     match parse_file_e file_parser input with
     | Ok ans -> ans
     | Error err ->
       failwith @@ CCFormat.sprintf "failed: %a" CCParse.Error.pp err
 end
 
+module Part1 = struct
+  let rec score (l : int list) =
+    match l with
+    | [] -> 0
+    | [ _ ] -> 1
+    | _ :: sc -> 2 * score sc
+
+  let score_list (card : Syntax.t) =
+    let have = card.have in
+    let winning = card.winners in
+    let matches =
+      CCList.filter (fun x -> CCList.exists (fun y -> y = x) have) winning
+    in
+    score matches
+
+  let ans card = CCList.fold_left ( + ) 0 (CCList.map score_list card)
+end
+
 let () =
   CCFormat.printf "%a" (CCList.pp Syntax.pp)
     (Syntax.parse_file ~input:"input.txt")
 (* (Syntax._parse_string ~input:_test) *)
+
+let () =
+  let card = Syntax.parse_file ~input:"input.txt" in
+  CCFormat.printf "@.%a" CCFormat.Dump.(int) (Part1.ans card)
