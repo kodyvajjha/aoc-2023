0c55558859d38476215011236e4f1da0cf4435a1
Koundinya Vajjha <koundinya.vajjha@gmail.com>
2023-12-13 20:37:09 -0800

    day 13 part 2

--- day13/day13.py
+++ day13/day13.py
@@ -4,9 +4,6 @@ with open(file_path,'r') as file:
 
 grids = [str(line).split('\n') for line in lines]
 
-# print(grids[0])
-
-
 def rowcheck(pattern):
     return [row for row in range(1,len(pattern)) if all([r1 == r2 for r1,r2 in zip(reversed(pattern[:row]),pattern[row:])])]
 
@@ -21,3 +18,19 @@ def part1():
   return (sum(colnums)+ 100 * sum(rownums))
 
 print(part1())
+
+
+# part2, following u/coolioasjulio's solution. (too tired to attempt part 2 - I want to eat ice cream and sleep.)
+def num_h_refl_mismatch(pattern: list, row):
+    return sum(sum(1 for c1, c2 in zip(r1, r2) if c1 != c2) for r1, r2 in zip(reversed(pattern[:row]), pattern[row:]))
+
+def num_v_refl_mismatch(pattern: list, col):
+    return sum(sum(1 for c1, c2 in zip(reversed(row[:col]), row[col:]) if c1 != c2) for row in pattern)
+
+part2 = 0
+for pattern in grids:
+    score = next((100*r for r in range(1, len(pattern)) if num_h_refl_mismatch(pattern, r) == 1), 0)
+    if score == 0:
+        score = next((c for c in range(1, len(pattern[0])) if num_v_refl_mismatch(pattern, c) == 1), 0)
+    part2 += score
+print(part2)
