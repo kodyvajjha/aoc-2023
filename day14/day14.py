9e28bff5bfdea1ee7446e86abd8a387f16f06cb0
Koundinya Vajjha <koundinya.vajjha@gmail.com>
2023-12-14 22:13:14 -0800

    wip

--- day14/day14.py
+++ day14/day14.py
@@ -1,18 +1,27 @@
-file_path = './day14/input.txt' 
+file_path = './day14/input_test.txt' 
 with open(file_path,'r') as file:
   lines = file.read().split("\n")
 
-grids = [str(line).split('\n') for line in lines]
-# print(lines)
-cols = [''.join(map(str,x)) for x in list(zip(*lines))]
-# print(cols)
+grids = [str(line).split('\n')[0] for line in lines]
 
-def pushrocks(lst):
+cols = [''.join(map(str,x)) for x in list(zip(*lines))]
+
+
+def pushrocks_left(lst):
   return '#'.join(map(lambda l: ''.join(sorted(l,reverse=True)),lst.split('#')))
+
+def pushrocks_right(lst):
+  return '#'.join(map(lambda l: ''.join(sorted(l)),lst.split('#')))
+
+def push_west(cols):
+  return list(map(pushrocks_left,cols))
+
+def push_east(cols):
+  return list(map(pushrocks_right,cols))
 
 
 def part1():
-  newcols = list(map(pushrocks,cols)) 
+  newcols = list(map(pushrocks_left,cols)) 
   # numos = []
   ans = 0
   for pos in range(0,len(cols)):
@@ -20,5 +29,8 @@ def part1():
     ans += (len(cols)-pos)*(len([col[pos] for col in newcols if col[pos] == 'O']))
   return ans 
 
-# print(list(map(lambda x: cussort(list(x)), splitstring(cols[6]))))
 print(part1())
+print(grids) # original 
+print(cols)
+print([''.join(map(str,x)) for x in list(zip(*cols))])
+print(list(map(pushrocks_left,grids)))
